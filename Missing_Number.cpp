816cd958b6c4f18f5d46228059f5e732420f64e3
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-11 15:55:09 +0530

    commit mising number file

---
+++ Missing_Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int missingNumber(vector<int>& nums) {
+            for(int i = 0;i<=nums.size();i++){
+                int flag = 0;
+                for (int j =0;j<nums.size();j++){
+                    if(nums[j] == i){
+                        flag = 1;
+                        break;
+                    }
+    …
+            
+            
+        }
+    };
